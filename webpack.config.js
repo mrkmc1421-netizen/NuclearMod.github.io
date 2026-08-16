d2038be4aba6c2a63d5e18077cb486ff9a1764b1
mrkmc1421-netizen <mrkmc1421@gmail.com>
2026-08-16 02:02:34 -0400

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'production'
+    ? 'production'
+    : 'development',
+
+  entry: './src/index.jsx',
+
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'js/[name].[contenthash].js',
+    clean: true,
+    publicPath: '/'
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff2?|ttf|eot)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name][ext]'
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './static/index.html'
+    })
+  ],
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'static')
+    },
+    port: 8601,
+    hot: true,
+    historyApiFallback: true
+  },
+
+  devtool: 'source-map'
+};
